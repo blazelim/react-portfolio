13ab04904f18e65afb2fb41546eb77652d932060
Blaze Lim <blazebentleycolim@gmail.com>
2021-09-06 00:02:21 -0700

    added resume section placeholdeR

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,9 +1,48 @@
 import React from 'react';
+import downloadResume from '../../assets/blazelim_resume.txt';
 
-function Resume() {
-    return(
-        <h1>Resume Section rendered</h1>
+// resume export function
+export default function Resume() {
+    return (
+        <div className="resume">
+            <div className="download-container">
+               <a className="download" href={downloadResume} download>Download Resume</a>
+            </div>
+            <div>
+                <h2>Front-End</h2>
+                <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>Javascript</li>
+                    <li>Jquery</li>
+                    <li>Bootstrap</li>
+                    <li>Pure CSS</li>
+                    <li>React</li>
+                </ul>
+            </div>
+            <div>
+                <h2>Back-End</h2>
+                <ul>
+                    <li>NodeJS</li>
+                    <li>Express</li>
+                    <li>MySQL</li>
+                    <li>MongoDB</li>
+                    <li>Mongoose</li>
+                    <li>Model View Controllers</li>
+                    <li>Progressive Web Applications</li>
+                    <li>Twilio NodeJS</li>
+                </ul>
+            </div>
+            <div>
+                <h2>Development</h2>
+                <ul>
+                    <li>Node</li>
+                    <li>Git</li>
+                    <li>Jest</li>
+                    <li>Github</li>
+                    <li>Heroku</li>
+                </ul>
+            </div>
+        </div>
     )
 }
-
-export default Resume
