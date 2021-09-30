47cb1f10830e910462845a0b25423a91c65dce99
Blaze Lim <blazebentleycolim@gmail.com>
2021-09-30 16:38:57 -0700

    changed linkedin link

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -11,7 +11,7 @@ export default function Footer() {
     return (
         <footer className="footer">
             <a className="footer-link" href="https://github.com/blazelim">{ghIcon}</a>
-            <a className="footer-link" href="https://www.linkedin.com/in/blaze-bentley-814852187/">{repoIcon}</a>
+            <a className="footer-link" href="https://www.linkedin.com/in/blaze-lim-814852187/">{repoIcon}</a>
         </footer>
     )
 }
